99a923bd22fdf3dc38d377fcc01953fb8ad810a9
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2024-10-19 05:40:36 +0600

    Creating Function to login to IUT WEB -- added ids,passwords

---
+++ LoginToIutUser.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+students = [
+    {'id' : 'mahadiahmed', 'password' : 'Mahadisea123'},
+    {'id' : 'hasibulkarim', 'password' : 'Hasrim809'},
+    {'id' : 'ridwankhan', 'password' : 'Ridhan736'},
+    {'id' : 'muhtasimzawad', 'password' : 'Muhwad445'}
+]
