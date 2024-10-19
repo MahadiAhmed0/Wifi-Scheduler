b5d3556cd36574b0f115730a48b80150bb50bc9e
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2024-10-19 06:11:30 +0600

    changed uesrId and passowrd with random text

--- LoginToIutUser.py
+++ LoginToIutUser.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 students = [
-    {'id': 'mahadiahmed', 'password': 'Mahadisea123'},
-    {'id': 'hasibulkarim', 'password': 'Hasrim809'},
-    {'id': 'ridwankhan', 'password': 'Ridhan736'},
-    {'id': 'muhtasimzawad', 'password': 'Muhwad445'}
+    {'id': 'userName', 'password': 'userPassword'},
+    {'id': 'userName', 'password': 'userPassword'},
+    {'id': 'userName', 'password': 'userPassword'},
+    {'id': 'userName', 'password': 'userPassword'}
 ]
 
 def login_to_iut_user(username, password):
